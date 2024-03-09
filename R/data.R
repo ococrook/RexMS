c581ddceb8425b2fddde1873d791a43b33e5d143
Oliver M. Crook <ococrook@gmail.com>
2024-03-09 18:34:16 +0000

    more updates with data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' This is data to be included in my package
+#'
+#' @name BRD4_apo
+#' @description
+#' 
+#' A small HDX-MS dataset for BRD4 in apo state
+#' 
+#' @docType data
+#' @author My Name \email{ococrook@gmail.com}
+#' @keywords data
+NULL
+
+#' This is data to be included in my package
+#'
+#' @name BRD4_ibet
+#' @description
+#' 
+#' A complete HDX-MS dataset for BRD4 in complex with IBET151
+#' 
+#' @docType data
+#' @author My Name \email{ococrook@gmail.com}
+#' @keywords data
+NULL
